ca63b489a5686c5b393c325b9f76cc4d7db2289a
salsafadhilah <37093417+salsafadhilah@users.noreply.github.com>
2018-03-20 10:33:48 +0700

    Add files via upload

---
+++ perulangan(salsa).py
@@ -0,0 +1,25 @@
+print("              Perulangan \n \n")
+
+print ("1. Foor Loop \n")
+
+#program1
+i = 10
+for i in range (10):
+	print(i + 3)
+	i = i + 1
+print"\n"
+#program2
+for huruf in 'PROGRAM PYTHON':
+	print(huruf)
+print"\n"
+#program3
+pelajaran = ('Sosiologi', 'Geografi', 'Psikologi')
+for belajar in pelajaran:
+	print("IPS : ", belajar)
+	
+print("\n 2. While Loop \n")
+i=1
+while (i<15):
+	print (i)
+	i = i + 1
+	
